439d3c712b0e0e2ccc34e9d8847234343a7ab21a
Nicholas Amancio <nick.amancio44@gmail.com>
2024-09-19 22:09:09 -0700

    listed my goals for the challenge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,15 @@ const Home = () => {
         <>
             <div className="home">
                 <h1>69 Soft++</h1>
+                <ul>
+                    <li>3 boba drinks</li>
+                    <li>Eat out thrice a week</li>
+                    <li>2 Days of Rock Climbing</li>
+                    <li>Stretch Everyday</li>
+                    <li>Leetcode</li>
+                    <li>Chess Puzzle</li>
+                    <li>Duolingo</li>
+                </ul>
             </div>
         </>
     );
