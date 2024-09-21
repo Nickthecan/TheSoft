cbe6b0ddc130b8c11c88d520c9c2513f091e195b
Nicholas Amancio <nick.amancio44@gmail.com>
2024-09-20 21:37:49 -0700

    turned the unordered list into a checkbox list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,15 +6,32 @@ const Home = () => {
         <>
             <div className="home">
                 <h1>69 Soft++</h1>
-                <ul>
-                    <li>3 boba drinks</li>
-                    <li>Eat out thrice a week</li>
-                    <li>2 Days of Rock Climbing</li>
-                    <li>Stretch Everyday</li>
-                    <li>Leetcode</li>
-                    <li>Chess Puzzle</li>
-                    <li>Duolingo</li>
-                </ul>
+                <form action="">
+                    <input type="checkbox" id="boba1" />
+                    <label htmlFor="boba">boba drink 1</label><br/>
+                    <input type="checkbox" id="boba2" />
+                    <label htmlFor="boba">boba drink 2</label><br/>
+                    <input type="checkbox" id="boba3" />
+                    <label htmlFor="boba">boba drink 3</label><br/>
+                    <input type="checkbox" id="eat1" />
+                    <label htmlFor="eat">Eat out 1</label><br/>
+                    <input type="checkbox" id="eat2" />
+                    <label htmlFor="eat">Eat out 2</label><br/>
+                    <input type="checkbox" id="eat3" />
+                    <label htmlFor="eat">Eat out 3</label><br/>
+                    <input type="checkbox" id="rock1" />
+                    <label htmlFor="rock">Day 1 of Rock Climbing</label><br/>
+                    <input type="checkbox" id="rock2" />
+                    <label htmlFor="rock">Day 2 of Rock Climbing</label><br/>
+                    <input type="checkbox" id="stretch" />
+                    <label htmlFor="stretch">Stretch Everyday</label><br/>
+                    <input type="checkbox" id="leetcode" />
+                    <label htmlFor="leetcode">Leetcode</label><br/>
+                    <input type="checkbox" id="chess" />
+                    <label htmlFor="chess">Chess Puzzle</label><br/>
+                    <input type="checkbox" id="duolingo" />
+                    <label htmlFor="duolingo">Duolingo</label><br/>
+                </form>
             </div>
         </>
     );
