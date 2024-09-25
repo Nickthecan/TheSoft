e20db604ae920308daec101e0caef1175b739d09
Nicholas Amancio <nick.amancio44@gmail.com>
2024-09-24 23:35:37 -0700

    Did a few changes

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -3,9 +3,11 @@ import "../styles/Checkbox.css"
 const Checkbox = ( {id, nameForCheckBox} ) => {
     return(
         <>
-            <div className="box-checkbox">
-                <input type="checkbox" id={id} />
-                <label htmlFor={id}>{nameForCheckBox}</label><br/>
+            <div className="home">
+                <div className="box-checkbox">
+                    <input className="check" type="checkbox" id={id} />
+                    <label htmlFor={id}>{nameForCheckBox}</label><br/>
+                </div>
             </div>
         </>
     );
